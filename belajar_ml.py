4506b6b19815738ebe965b3f449da7f24207b180
Mutofa <mustofa562@gmail.com>
2025-12-19 14:19:12 +0700

    belajar lg

--- belajar_ml.py
+++ belajar_ml.py
@@ -59,3 +59,68 @@ prediksi = model.predict(X_test)
 
 print("Prediksi:", prediksi)
 print("Jawaban asli:", y_test)
+
+#latihan lagi 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Data
+X = np.array([[1], [2], [3], [4]])
+y = np.array([10, 20, 30, 40])
+
+# Model
+model = LinearRegression()
+
+# Train
+model.fit(X, y)
+
+# Predict
+print(model.predict([[8]]))
+
+#
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([10, 20, 30, 40, 50])
+
+# Bagi data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=1
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print("X_test:", X_test)
+print("Prediksi:", model.predict(X_test))
+print("Asli:", y_test)
+
+from sklearn.metrics import r2_score
+
+r2 = r2_score(y_test, model.predict(X_test))
+print("R2 score:", r2)
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([12, 19, 33, 38, 55])  # data agak acak
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=1
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+print("X_test:", X_test)
+print("Prediksi:", pred)
+print("Asli:", y_test)
+print("R2:", r2_score(y_test, pred))
+
